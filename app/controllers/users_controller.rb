824db5885a56231b21a720cc47e2ea1bebee65f8
Jason Low <lowjiashen@gmail.com>
2016-05-31 14:50:13 +1000

    Added controller and db and some views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  # Index page '/'
+  def home
+    @users = User.all
+  end
+
+  def edit
+
+  end
+
+  def create
+    user = User.all
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
+
+  get '/sign_up' => 'users#new'
+  post '/users' => 'users#create'
+
+  # resource :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
